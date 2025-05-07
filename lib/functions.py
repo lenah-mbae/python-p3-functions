c1807e551e385feb5ea0f83aa71dfeedbebfb64a
Lenah Mbae <lenahmbae22@gmail.com>
2025-05-07 16:59:25 +0300

    function tests

--- lib/functions.py
+++ lib/functions.py
@@ -2,15 +2,25 @@
 
 def greet_programmer():
     pass
+    print(f"Hello, programmer!")
 
-def greet(name):
+def greet(name = "Mark"):
     pass
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
     pass
+    print(f"Hello, {name}!")
+greet_with_default()
+greet_with_default("Sunny")
+
 
 def add(num1, num2):
     pass
+    return num1 + num2
+    print(add)
 
 def halve(number):
     pass
+    return number/2
+    print(halve)
